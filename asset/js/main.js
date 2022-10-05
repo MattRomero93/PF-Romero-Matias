791a1f2e022a23b222af139733d7c6498b28bee2
Matias Romero <mattromero93@gmail.com>
2022-10-05 16:35:23 -0300

    Agregado slide, correciones de color y js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,53 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
 
+
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle('active');
     toggleMenuElement.classList.toggle('active');
 });
+
+
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnPrev = document.querySelector("#btn-left");
+const btnNext = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft ="-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll('.slider__section');
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft ="-100%";
+    }, 500);
+}
+
+btnNext.addEventListener('click', function(){
+    Next();
+});
+
+btnPrev.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 6000);
